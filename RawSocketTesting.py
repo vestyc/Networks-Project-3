06ec1fb22c3a3452b99c87e6997bbfe24b6b906e
Johnny <tranjohnny64@gmail.com>
2015-11-24 16:35:24 -0600

    update
    all except arp

--- RawSocketTesting.py
+++ RawSocketTesting.py
@@ -5,11 +5,11 @@ import struct
 HOST = socket.gethostbyname(socket.gethostname())
 
 # create a raw socket and bind it to the public interface
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW)
 s.bind((HOST, 0))
 
 # Include IP headers
-s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+#s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
 # receive all packages
 s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
@@ -17,7 +17,7 @@ s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 while(True):
     # receive a package
     packet = s.recvfrom(65565)
-         
+    
     #packet string from tuple
     packet = packet[0]
          
@@ -54,8 +54,8 @@ while(True):
         doff_reserved = tcph[4]
         tcph_length = doff_reserved >> 4
              
-        print("TCP")     
-        print ('Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length))
+        #print("TCP")     
+        #print ('Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length))
             
         h_size = iph_length + tcph_length * 4
         data_size = len(packet) - h_size
@@ -63,7 +63,11 @@ while(True):
         #get data from the packet
         data = packet[h_size:]
              
-        #print ('Data : ' + data.strip().decode('utf-8'))
+        #check for http
+        if dest_port == 80:
+            #unpack http
+            http = packet[iph_length+20:len(packet)]
+            print(http)
 
     elif protocol == 17:
         udp_header = packet[iph_length:iph_length+8]
@@ -75,7 +79,21 @@ while(True):
         length = udph[2]
         checksum = udph[3]
 
-        print("UDP")
-        print('Source Port: ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Length: ' + str(length) + ' Checksum : ' + str(checksum))
+        #print("UDP")
+        #print('Source Port: ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Length: ' + str(length) + ' Checksum : ' + str(checksum))
 
-    
+    elif protocol == 1:
+        icmp_header = packet[iph_length:iph_length+8]
+        icmph = struct.unpack('!BBHHH', icmp_header);
+
+        #unpacking
+        icmp_type = icmph[0]
+        icmp_code = icmph[1]
+        icmp_checksum = icmph[2]
+        icmp_identifier = icmph[3]
+        icmp_seqnum = icmph[4]
+
+        #print("ICMP")
+        #print('Type: ' + str(icmp_type) + ' Code: ' + str(icmp_code) + ' Checksum: ' + str(icmp_checksum))
+        #print('Identifier: ' + str(icmp_identifier) + ' Sequence Number: ' + str(icmp_seqnum))
+        
